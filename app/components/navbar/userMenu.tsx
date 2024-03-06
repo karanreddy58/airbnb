0a5b6cca9f9466e6c05786fc78b713f1df27649d
karan <K@ran558>
2024-03-06 00:16:24 -0500

    completed login functionality

--- app/components/navbar/userMenu.tsx
+++ app/components/navbar/userMenu.tsx
@@ -5,9 +5,11 @@ import Avatar from "../Avatar";
 import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
+import useLoginModal from "@/app/hooks/useLoginModal";
 
 const UserMenu = () => {
     const registerModal = useRegisterModal();
+    const loginModal = useLoginModal();
     const [isOpen, setIsOpen] = useState(false);
     
 
@@ -75,7 +77,7 @@ const UserMenu = () => {
                     className="flex flex-col cursor-pointer">
                     <>
                         <MenuItem
-                        onClick={()=>{}}
+                        onClick={loginModal.onOpen}
                         label="Login"/>
 
                         <MenuItem
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import  {Nunito} from "next/font/google"
 import Navbar from "./components/navbar/navbar";
 import RegisterModal from "./components/modals/RegisterModal";
 import ToasterProvider from "./providers/ToasterProvider";
+import LoginModal from "./components/modals/LoginModal";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ToasterProvider/>
+        <LoginModal/>
         <RegisterModal/>
         <Navbar />
         {children}</body>
